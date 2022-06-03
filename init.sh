20d6fa65e06ce67490bf8eaeba5e20f57bda501f
Arthur Liu <yikliu@me.com>
2022-06-03 01:53:27 -0700

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,15 +4,13 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f ~/dotfiles
 ln -s ${dir} ~/dotfiles
 rm -f ~/.bashrc
-ln -s ${dir}/rc ~/.bashrc
+ln -s ${dir}/bashrc ~/.bashrc
 rm -f ~/.bash_profile
 ln -s ${dir}/profile ~/.bash_profile
 rm -f ~/.vimrc
 ln -s ${dir}/vimrc ~/.vimrc
 rm -f ~/.tmux.conf
 ln -s ${dir}/tmux ~/.tmux.conf
-rm -f ~/.tridactylrc
-ln -s ${dir}/tridactylrc ~/.tridactylrc
 rm -f ~/.zshrc
 ln -s ${dir}/zshrc ~/.zshrc
 
@@ -20,6 +18,10 @@ if [ -n "$ZSH_VERSION" ]; then
 	source ~/.zshrc
 elif [ -n "$BASH_VERSION" ]; then
 	source ~/.bashrc
+else
+    echo 'unknown shell'
+fi
 
 source ${dir}/myalias
 source ~/amazon
+
