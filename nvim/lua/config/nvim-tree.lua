a6985e8013985d949d141792ada93fa96faca51c
Yikun Liu <yikunliu@amazon.com>
2023-10-23 08:59:30 -0700

    update

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -13,22 +13,22 @@ nvim_tree.setup {
   update_cwd = false,
   view = {
     width = 40,
-    hide_root_folder = false,
+    -- hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
     relativenumber = false,
     signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
+    -- mappings = {
+    --  custom_only = false,
+    --  list = {
+    --    -- user mappings go here
+    --  },
+    --},
   },
-  remove_keymaps = {
-   "H",
-  },
+  -- remove_keymaps = {
+  --  "H",
+  -- },
   renderer = {
     indent_markers = {
       enable = false,
