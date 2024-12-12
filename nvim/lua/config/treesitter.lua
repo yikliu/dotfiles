1a59e17b335f143911ac4c7fe7b5968966cc27e0
Arthur Liu <yikliu@mini-me.lan>
2024-12-11 19:49:50 -0800

    update

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,9 +1,19 @@
 require("nvim-treesitter.configs").setup {
     ensure_installed = { "python", "java", "cpp", "lua", "vim", "typescript", "markdown", "kotlin", "markdown_inline", "html", "bash" },
-    ignore_install = { "org" }, -- List of parsers to ignore installing
+    ignore_install = {}, -- List of parsers to ignore installing
     sync_install = true,
     highlight = {
         enable = true, -- false will disable the whole extension
     },
     indent = { enable = true },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = { 'src/parser.c', 'src/scanner.c' },
+  },
+  filetype = 'org',
 }
